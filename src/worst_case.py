6836d13e576e79e75682456aff918f1df47d54c2
Daanish Mahajan <daanishmahajan@Daanishs-MacBook-Pro.local>
2025-01-04 22:43:54 +0530

    4/1/25

---
+++ src/worst_case.py
@@ -0,0 +1,31 @@
+import os
+
+depth = 10000
+ofile = open(f'../test/data/gfa_files/worst_case({depth}).gfa', 'w')
+
+tot_nodes = 4 * depth
+tot_edges = 6 * depth - 2
+ch = 26
+
+dict = {}
+
+for i in range(tot_nodes):
+    s = '' if i > 0 else 'A'
+    x = i
+    while(x > 0):
+        s += chr(65 + (x % ch))
+        x //= ch
+    dict[i] = s
+    print('S', s, '*', sep = '\t', file = ofile)
+
+for i in range(depth):
+    print('L', dict[4 * i], '+', dict[4 * i + 2], '+', sep = '\t', file = ofile)
+    print('L', dict[4 * i], '+', dict[4 * i + 3], '+', sep = '\t', file = ofile)
+    print('L', dict[4 * i + 2], '+', dict[4 * i + 1], '+', sep = '\t', file = ofile)
+    print('L', dict[4 * i + 3], '+', dict[4 * i + 1], '+', sep = '\t', file = ofile)
+
+    if i > 0:
+        print('L', dict[4 * (i - 1)], '+', dict[4 * i], '+', sep = '\t', file = ofile)
+        print('L', dict[4 * i + 1], '+', dict[4 * (i - 1) + 1], '+', sep = '\t', file = ofile)
+
+ofile.close()
